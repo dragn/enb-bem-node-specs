2ba183c8636a95c735fa2dcaecb66a0fece4991c
Daniel Sabelnikov <dsabelnikov@gmail.com>
2015-02-27 15:55:11 +0300

    remove should.js, add chai`s expect and assert to global scope

--- examples/server.blocks/test-block/test-block.spec.js
+++ examples/server.blocks/test-block/test-block.spec.js
@@ -3,7 +3,8 @@ modules.define('spec', ['test-block'], function(provide, testBlock) {
     describe('test-block block', function() {
 
         it('should return current working directory', function() {
-            chai.expect(testBlock).to.be.defined;
+            assert.isDefined(testBlock);
+            expect(testBlock).to.be.defined;
             testBlock.getCurrentPath().should.equal(process.cwd());
         });
 
